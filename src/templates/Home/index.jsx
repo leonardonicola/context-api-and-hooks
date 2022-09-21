d7e0010e5926e2bab260747e4a403224b93b352f
Leonardo Nicola Soares <leonardo.soares@kumon.com.br>
2022-09-21 16:09:53 -0300

    Context API

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,24 @@
+import { Button } from '../../components/Button';
+import { Heading } from '../../components/Heading';
+import { useCounterContext } from '../../contexts/CounterContext';
+import './styles.css';
+
 export const Home = () => {
+  const [state, actions] = useCounterContext();
+
   return (
-    <div>
-      <h1>Oi</h1>
+    <div className="main">
+      <div>
+        <Heading text="Context API on React!" />
+        <Heading text={state.counter} />
+      </div>
+      <div>
+        <Button text="INCREASE" onButtonClick={actions.increase} />
+        <Button text="DECREASE" onButtonClick={actions.decrease} />
+        <Button text="RESET" onButtonClick={actions.reset} />
+        <Button text="SET 10" onButtonClick={() => actions.setCounter({ counter: 10 })} />
+        <Button disabled={state.loading} text="ASYNC INCREASE" onButtonClick={actions.asyncIncrease} />
+      </div>
     </div>
   );
 };
